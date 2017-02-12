bb1281266c39fe8cfea0db867a0ab8b075fd0b9e
Simon Garnier <garnier@njit.edu>
2017-02-12 10:52:10 -0500

    Updates mid_dist function: adds argument check, fixes documentation.

--- R/mid_dist.R
+++ R/mid_dist.R
@@ -1,24 +1,30 @@
-#' @title Mid-distance function
+#' @title Mid-Distance Function
 #'
-#' @description This function models the distance between two
-#'              bridges along a common arm.
+#' @description This function models the distance between two bridges along a
+#'  common arm.
 #'
-#' @param d1 Distance of one bridge from apex.
+#' @param d1 A numeric vector of distances of first bridge from apex in cm.
 #'
-#' @param d2 Distance of other bridge from apex.
+#' @param d2 A numeric vector of distances of second bridge from apex in cm.
 #'
-#' @param theta Angle of apex (in radians)
+#' @param theta A numeric vector of angles of apex in rad.
 #'
-#' @param Dmax Maximum possible distance of a bridge from apex.
+#' @param Dmax A numeric vector of the maximum possible distances of a bridge
+#'  from apex in cm.
 #'
-#' @return mid-distance
+#' @return A vector of the same length as \code{d1, d2, theta}.
 #'
 #' @author Jason Graham, \email{jason.graham@@scranton.edu}
 #'
 #' @export
 #'
-mid_dist <- function(d1, d2, theta, Dmax){
+mid_dist <- function(d1, d2, theta, Dmax) {
+  if (!all(is.numeric(d1), is.numeric(d2), is.numeric(theta), is.numeric(Dmax)))
+    stop("All arguments must be numeric")
 
-  return(abs((1/cos(0.5*theta))*(Dmax - d1 - d2)))
+  l <- lengths(list(d1, d2, theta, Dmax))
+  if (!all(((l == 1) + (l == max(l))) > 0))
+    stop("All arguments should be of either length 1 or of the same length.")
 
+  abs((1 / cos(0.5 * theta)) * (Dmax - d1 - d2))
 }
