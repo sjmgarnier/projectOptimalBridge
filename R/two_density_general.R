950e808dd529a9184dbc0aaaee398e559ec80d2e
Simon Garnier <garnier@njit.edu>
2017-02-12 17:59:16 -0500

    Fixes two_density_general function and documentation.

--- R/two_density_general.R
+++ R/two_density_general.R
@@ -1,57 +1,48 @@
-#' @title Density Function for Two-apex Apparatus
+#' @title Density Function for Two-Apex Apparatus
 #'
-#' @description This function models the density as a function of
-#'              distance for the two-apex appratus.
+#' @description This function computes the density as a function of distance for
+#'  the two-apex appratus.
 #'
-#' @param d vector of distances values
+#' @param d A numeric dector of distances values in cm.
 #'
-#' @param angle Angle of apex (in degrees)
+#' @param angle A vector of angles of apex in deg.
 #'
-#' @param ant.dens Density of ants, default value =  2.2
+#' @param ant.dens A scalar for the density of ants in ants/cm (default: 2.2).
 #'
-#' @param LT Overall trail length, default value = 100 (cm)
+#' @param LT A scalar for the overall trail length in cm (default: 100).
 #'
-#' @param L0 Length of one arm, default value = 22 (cm)
+#' @param L0 A scalar for the length of the apparatus arm in cm (default: 22).
 #'
-#' @param ln Length of avg ant default value = 0.691 (cm)
+#' @param wA A scalar for the width of the arms of the apparatus in cm (default: 3.3).
 #'
-#' @param wn  Width of avg ant default value = 0.107 (cm)
+#' @param ln A scalar for the average length of an ant in cm (default:0.691).
 #'
-#' @param wA Width of apparatus, default value = 3.3 (cm)
+#' @param wn A scalar for the average width of an ant in cm (default: 0.107).
 #'
-#' @param alpha Free fitting parameter value from Reid et al, default value 17.02
+#' @param alpha A scalar for the free fitting parameter value from Reid et al.
+#'  (default: 17.02).
 #'
-#' @return Density \code{rho}
+#' @return A vector of the of the same length as \code{d, angle}.
 #'
 #' @author Jason Graham, \email{jason.graham@@scranton.edu}
 #'
+#' @author Simon Garnier, \email{garnier@@njit.edu}
+#'
 #' @export
 #'
-two_density_general <- function(d,
-                                angle,
-                                ant.dens = 2.2,
-                                LT = 100,
-                                L0 = 22,
-                                ln = 0.691,
-                                wn = 0.107,
-                                wA = 3.3,
-                                alpha = 17.02){
+two_density_general <- function(d, angle, ant.dens = 2.2, LT = 100, L0 = 22,
+                                wA = 3.3, ln = 0.691, wn = 0.107, alpha = 17.02) {
 
-  # Function defining density for two-bridge model
+  theta <- pi / 180 * angle
+  wT <- 4.799 * angle ^ -0.5014
+  LAi <- L0 - 0.5 * wA / tan(0.5 * theta)
+  N <- ant.dens * (LT + 3 * LAi)
+  Dmax <- LAi * cos(0.5 * theta)
 
-  ### parameters ###
-  theta <- pi/180*angle  # angle in radians
-  wT <- 4.799*(angle)^(-0.5014) # ratio of bridge width/length (value from Reid et al)
-  LAi <- L0 - 0.5*wA/tan(0.5*theta) # (cm)
-  N <- ant.dens*(LT+3*LAi)
-  Dmax <- LAi*cos(0.5*theta)
+  b <- 2 * tan(0.5 * theta) * d
+  nb <- wT / (ln * wn) * (1 - wT * tan(0.5 * theta)) * b ^ 2
+  f <- LT + (1 - d[1] / Dmax) * LAi + (1 - d[2] / Dmax) * LAi +
+    mid_dist(d[1], d[2], theta, Dmax) + b[1] + b[2]
 
-  ### specification of function ###
-  b <- 2*tan(0.5*theta)*d
-  nb <- wT/(ln*wn)*(1 - wT*tan(0.5*theta))*b^2
-  f <- LT + (1 - d[1]/Dmax)*LAi + (1 - d[2]/Dmax)*LAi + mid_dist(d[1],d[2],theta,Dmax) + b[1] + b[2]
-
-  rho <- (N - nb[1]/alpha - nb[2]/alpha)/f  # density function
-
-  return(rho)
+  (N - nb[1] / alpha - nb[2] / alpha) / f
 }
